8bebc7f7474d81ee01979e42fc96ca5c6887fbdc
mherkh <mherkhachatryan35912@gmail.com>
2024-08-12 08:02:44 +0100

    added executable for mac and windows

--- watermark.py
+++ watermark.py
@@ -2,6 +2,7 @@ import os
 from pathlib import Path
 import argparse
 from tqdm import tqdm
+import sys
 
 from _watermark import add_watermark
 from compressor import compress_images
@@ -9,10 +10,22 @@ import warnings
 
 warnings.filterwarnings("ignore")
 
-if __name__ == "__main__":
+
+def resource_path(relative_path):
+    """ Get absolute path to resource, works for dev and for PyInstaller """
+    try:
+        # PyInstaller creates a temp folder and stores path in _MEIPASS
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
+
+
+def parse_arguments():
     parser = argparse.ArgumentParser(
         description="Add watermarks to batch of images. Original images won't be affected. A new copy will be created")
-    parser.add_argument('-i', '--images_path', type=str, required=True,
+    parser.add_argument('-i', '--images_path', type=str,
                         help='The path to the folder containing images.')
     parser.add_argument('-o', '--opacity', type=float, default=0.8,
                         help='The opacity of the watermarks. Default is 0.8.')
@@ -25,13 +38,26 @@ if __name__ == "__main__":
     parser.add_argument('--compress_only', action='store_true',
                         help='Only compress the images, do not add a watermark.')
 
-    args = parser.parse_args()
+    # Ignore unknown arguments
+    args, unknown = parser.parse_known_args()
+    if unknown:
+        print(f"Warning: Unknown arguments ignored: {unknown}")
+    return args
 
-    watermarks_path = Path("img")
+
+def main():
+    args = parse_arguments()
+
+    # If running as executable and no images_path provided, use the executable's directory
+    if getattr(sys, 'frozen', False) and not args.images_path:
+        images_path = Path(os.path.dirname(sys.executable))
+    else:
+        images_path = Path(args.images_path) if args.images_path else Path.cwd()
+
+    watermarks_path = Path(resource_path("img"))
     logo_path = watermarks_path / "Space_42_logo.png"
     insta_logo = watermarks_path / "instagram.png"
 
-    images_path = Path(args.images_path)
     output_path_watermarked = images_path / "watermarked"
     output_path_compressed_only = images_path / "compressed"
 
@@ -39,9 +65,15 @@ if __name__ == "__main__":
     os.makedirs(output_path_compressed_only, exist_ok=True)
 
     for img_name in tqdm(os.listdir(str(images_path))):
-        if img_name.lower().endswith(".jpg") or img_name.endswith(".png"):
+        if img_name.lower().endswith((".jpg", ".png")):
             if args.compress_only:
                 compress_images(images_path / img_name, output_path_compressed_only / img_name, args.quality)
             else:
                 add_watermark(images_path / img_name, logo_path, insta_logo, output_path_watermarked,
                               opacity=args.opacity, scale1=args.scale1, scale2=args.scale2, quality=args.quality)
+
+    print("Processing complete. Check the 'watermarked' or 'compressed' folder for output.")
+
+
+if __name__ == "__main__":
+    main()
